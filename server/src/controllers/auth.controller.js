8e81bbddee910f23466e8cde255016f6be12427e
Premium07 <premsagarg23@gmail.com>
2025-05-02 12:47:19 +0545

    Implement user signup with validation, password hashing, and JWT token generation; update server to handle URL-encoded data

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,7 +1,76 @@
-import { User } from "../models/user";
+import { User } from "../models/user.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
-  res.send("signup");
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter all fields",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const emailRegix = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegix.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists, Please use different email",
+      });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true, // prevents XCC attacks
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", // prevents CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    // Safely remove password
+    const userData = user.toObject();
+    delete userData.password;
+
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const login = async (req, res) => {
