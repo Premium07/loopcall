30f8fb8996464a697deab5a9ede196998e941a37
Premium07 <premsagarg23@gmail.com>
2025-05-04 14:53:18 +0545

    feat: implement routing for Home, Login, SignUp, Notifications, Chat, and Call pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import ChatPage from "./pages/ChatPage";
+import CallPage from "./pages/CallPage";
 
 const App = () => {
-  return <main></main>;
+  return (
+    <main className="h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
