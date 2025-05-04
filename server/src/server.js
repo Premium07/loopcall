36609fe53d7df28ff7b3991b4d074ff8bce7932d
Premium07 <premsagarg23@gmail.com>
2025-05-04 16:00:53 +0545

    feat: configure CORS to allow requests from client URL with credentials

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
